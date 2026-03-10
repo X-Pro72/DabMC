PlaceholderMC/PlaceholderMC

add setting to update minecraft instance setting to the latest available version
Signed-off-by: Trial97 <alexandru.tripon97@gmail.com>

launcher/minecraft/MinecraftLoadAndCheck.cpp
@@ -1,4 +1,5 @@
 #include "MinecraftLoadAndCheck.h"
+#include "Application.h"
 #include "MinecraftInstance.h"
 #include "PackProfile.h"
 
@@ -8,6 +9,14 @@ void MinecraftLoadAndCheck::executeTask()
 {
     // add offline metadata load task
     auto components = m_inst->getPackProfile();
+    if (m_inst->settings()->get("UseLatestMinecraftVersion").toBool()) {
+        if (APPLICATION->settings()->get("AutomaticJavaSwitch").toBool() && m_inst->settings()->get("AutomaticJava").toBool() &&
+            m_inst->settings()->get("OverrideJavaLocation").toBool()) {
+            m_inst->settings()->set("OverrideJavaLocation", false);
+            m_inst->settings()->set("JavaPath", "");
+        }
+        components->updateLatesMinecraft();
+    }
     if (auto result = components->reload(m_netmode); !result) {
         emitFailed(result.error);
         return;

launcher/minecraft/PackProfile.cpp
@@ -1069,3 +1069,20 @@ QList<ModPlatform::ModLoaderType> PackProfile::getModLoadersList()
     }
     return result;
 }
+
+void PackProfile::updateLatesMinecraft()
+{
+    const QString uid = "net.minecraft";
+    auto patch = getComponent(uid);
+    patch->waitLoadMeta();  // make sure we have latest versions
+    auto list = patch->getVersionList();
+    if (!list) {
+        return;
+    }
+
+    auto latest = list->getLatest();
+
+    qDebug() << "Change" << uid << "to" << latest.get();
+    setComponentVersion(uid, latest->descriptor(), true);
+    resolve(Net::Mode::Online);
+}

X-Pro72/DabMC

launcher/minecraft/MinecraftLoadAndCheck.cpp
@@ -1,4 +1,5 @@
 #include "MinecraftLoadAndCheck.h"
+#include "Application.h"
 #include "MinecraftInstance.h"
 #include "PackProfile.h"
 
@@ -8,6 +9,14 @@ void MinecraftLoadAndCheck::executeTask()
 {
     // add offline metadata load task
     auto components = m_inst->getPackProfile();
+    if (m_inst->settings()->get("UseLatestMinecraftVersion").toBool()) {
+        if (APPLICATION->settings()->get("AutomaticJavaSwitch").toBool() && m_inst->settings()->get("AutomaticJava").toBool() &&
+            m_inst->settings()->get("OverrideJavaLocation").toBool()) {
+            m_inst->settings()->set("OverrideJavaLocation", false);
+            m_inst->settings()->set("JavaPath", "");
+        }
+        components->updateLatestMinecraft();
+    }
     if (auto result = components->reload(m_netmode); !result) {
         emitFailed(result.error);
         return;

launcher/minecraft/PackProfile.cpp
@@ -1063,3 +1063,20 @@ QList<ModPlatform::ModLoaderType> PackProfile::getModLoadersList()
     }
     return result;
 }
+
+void PackProfile::updateLatestMinecraft()
+{
+    const QString uid = "net.minecraft";
+    auto patch = getComponent(uid);
+    patch->waitLoadMeta();  // make sure we have latest versions
+    auto list = patch->getVersionList();
+    if (!list) {
+        return;
+    }
+
+    auto latest = list->getLatest();
+
+    qDebug() << "Change" << uid << "to" << latest.get();
+    setComponentVersion(uid, latest->descriptor(), true);
+    resolve(Net::Mode::Online);
+}